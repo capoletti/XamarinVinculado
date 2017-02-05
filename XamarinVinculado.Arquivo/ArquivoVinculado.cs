d24e5589bdc8c6190793ca8740ae80d58ba6e6a7
capoletti <cpoletti@gmail.com>
2017-02-05 20:10:39 -0200

    verso arquivo vinculado

---
+++ XamarinVinculado.Arquivo/ArquivoVinculado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace XamarinVinculado.Arquivo
+{
+	public class ArquivoVinculado
+	{
+		public string RetornarValor(int contador)
+		{
+			string valor = "arquivo: " + contador;
+
+#if __ANDROID__
+			valor += " - Android: " + Android.OS.Build.VERSION.Codename;
+#elif __IOS__
+			valor += " - iOS: " + UIKit.UIDevice.CurrentDevice.SystemVersion;
+			#endif
+
+			return valor;
+		}
+	}
+}
---
+++ XamarinVinculado.Droid/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinVinculado.Droid
+{
+	[Activity(Label = "XamarinVinculado.Droid", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 0;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button bAtualizar = FindViewById<Button>(Resource.Id.atualizar);
+			Button bLimpar = FindViewById<Button>(Resource.Id.limpar);
+			bAtualizar.Click += delegate { atualizar(); };
+			bLimpar.Click += delegate { limpar(); };
+		}
+
+		private void atualizar()
+		{
+			count++;
+			TextView tArquivo = FindViewById<TextView>(Resource.Id.textArquivo);
+			TextView tProjeto = FindViewById<TextView>(Resource.Id.textProjeto);
+			TextView tBiblioteca = FindViewById<TextView>(Resource.Id.textBiblioteca);
+			tArquivo.Text = (new Arquivo.ArquivoVinculado()).RetornarValor(count);
+		}
+
+		private void limpar()
+		{
+			count = 0;
+			TextView tArquivo = FindViewById<TextView>(Resource.Id.textArquivo);
+			TextView tProjeto = FindViewById<TextView>(Resource.Id.textProjeto);
+			TextView tBiblioteca = FindViewById<TextView>(Resource.Id.textBiblioteca);
+			tArquivo.Text = "Arquivo";
+			tProjeto.Text = "Projeto";
+			tBiblioteca.Text = "Biblioteca";
+		}
+	}
+}
+
